61c442e325b9ae5cc9346d20dc9f22feb1c6ed9f
Сол Коган <45554270+solkogan@users.noreply.github.com>
2019-12-24 13:29:42 +0600

    Add files via upload

--- cgi-bin/form.py
+++ cgi-bin/form.py
@@ -5,9 +5,6 @@ import subprocess
 form = cgi.FieldStorage()
 nick = form.getfirst("nick", "не задано")
 
-f=open(os.getcwd()+'/template/form.html','r', encoding="UTF-8")
-s=f.read()
-
 fileout = open(os.getcwd()+'/cgi-bin/sherlock/scan.txt', 'w')
 theproc = subprocess.Popen(os.getcwd()+"/cgi-bin/sherlock/sherlock.py "+nick, stdout=fileout, shell = True)
 theproc.communicate()
@@ -16,8 +13,12 @@ fileout.close()
 f=open(os.getcwd()+'/cgi-bin/sherlock/scan.txt','r', encoding="UTF-8")
 r=f.read()
 f.close()
-s=s.replace('%result%', r)
+
+f=open(os.getcwd()+'/template/form.html','r', encoding="UTF-8")
+htmltemplate=f.read()
 f.close()
+htmltemplate=htmltemplate.replace('%result%', r)
+
 print("Content-type: text/html\n")
-print(s)
+print(htmltemplate)
 
