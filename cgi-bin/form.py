785fa2f80cbe40b9a34d11615a309d7f5ebec998
Сол Коган <45554270+solkogan@users.noreply.github.com>
2019-12-24 15:44:59 +0600

    Add files via upload

--- cgi-bin/form.py
+++ cgi-bin/form.py
@@ -2,23 +2,31 @@ import os, cgi
 import sys
 import subprocess
 
+# Получаем GET параметр nick переданный из HTML формы
 form = cgi.FieldStorage()
-nick = form.getfirst("nick", "не задано")
+nick = form.getfirst("nick", "test")
 
+# Открываем на запись scan.txt куда выведем результаты работы скрипта sherlock
 fileout = open(os.getcwd()+'/cgi-bin/sherlock/scan.txt', 'w')
+# Запускаем внешний скрипт sherlock.py и передаем ему nick, результат работы скрипта идет
+# в файл scan.txt
 theproc = subprocess.Popen(os.getcwd()+"/cgi-bin/sherlock/sherlock.py "+nick, stdout=fileout, shell = True)
 theproc.communicate()
 fileout.close()
 
+# Снова открываем файл с результатами и читаем его в переменную r
 f=open(os.getcwd()+'/cgi-bin/sherlock/scan.txt','r', encoding="UTF-8")
 r=f.read()
 f.close()
 
+# Открываем HTML шаблон странички с результатами
 f=open(os.getcwd()+'/template/form.html','r', encoding="UTF-8")
 htmltemplate=f.read()
 f.close()
+# Заменяем в HTML шаблоне %result% на переменную r с результатами Шерлока
 htmltemplate=htmltemplate.replace('%result%', r)
 
+# Выводим в браузер
 print("Content-type: text/html\n")
 print(htmltemplate)
 
--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,6 +1,9 @@
 import os
+# Грузим из файла HTML шаблон с формой
 f=open(os.getcwd()+'/template/index.html','r', encoding="UTF-8")
 s=f.read()
 f.close()
+# Обязательно сперва выводим "Content-type: text/html\n"
 print("Content-type: text/html\n")
+# Выводим HTML шаблон
 print(s)
